2a9dae0017e978db791593dced7ceaaec7540ddc
Marshall Brown <marshall.brown@faithlife.com>
2021-05-07 10:08:22 -0700

    init commit

---
+++ setup.r
@@ -0,0 +1,8 @@
+# run ?datadrivencv::use_datadriven_cv to see more details
+datadrivencv::use_datadriven_cv(
+  full_name = "Marshall Daniel Brown",
+  data_location = "https://docs.google.com/spreadsheets/d/1NgJjkPKD5yTjUT6fJ8mDXFcY5DxA6Sj3sJeUCVbFcfQ/edit?usp=sharing",
+  pdf_location = "",
+  html_location = "",
+  source_location = "https://github.com/nstrayer/cv"
+)
